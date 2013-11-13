0de8f0037066a2037539234d2c197a947ac0ccab
Mark Stratis <mstratis@cardinalsolutions.com>
2013-11-13 15:27:47 -0500

    uncommented code

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -9,11 +9,11 @@ import models.*;
 
 public class Application extends Controller {
 
-//	@Before
-//	static void addDefaults() {
-//	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
-//	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
-//	}
+	@Before
+	static void addDefaults() {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
 	
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
