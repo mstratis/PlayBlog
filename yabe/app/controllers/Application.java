923c732a15f648558df627e33b607008de78fbf2
Mark Stratis <mstratis@cardinalsolutions.com>
2013-11-13 16:50:27 -0500

    view and post comments

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.*;
 import play.mvc.*;
 
 import java.util.*;
@@ -23,4 +24,19 @@ public class Application extends Controller {
         render(frontPost, olderPosts);
     }
 
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+    
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if(validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
+    }
+    
 }
--- yabe/app/models/Post.java
+++ yabe/app/models/Post.java
@@ -34,5 +34,13 @@ public class Post extends Model {
         this.save();
         return this;
     }
+    
+    public Post previous() {
+        return Post.find("postedAt < ? order by postedAt desc", postedAt).first();
+    }
+     
+    public Post next() {
+        return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+    }
  
 }
