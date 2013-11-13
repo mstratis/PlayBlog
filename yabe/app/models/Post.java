45a9619875414869da0d117e6ea0caac66789500
Mark Stratis <mstratis@cardinalsolutions.com>
2013-11-13 13:55:17 -0500

    created model and model tests

---
+++ yabe/app/models/Post.java
@@ -0,0 +1,38 @@
+package models;
+ 
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+ 
+@Entity
+public class Post extends Model {
+ 
+    public String title;
+    public Date postedAt;
+    
+    @Lob
+    public String content;
+    
+    @ManyToOne
+    public User author;
+    
+    @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+    public List<Comment> comments;
+     
+    public Post(User author, String title, String content) { 
+        this.comments = new ArrayList<Comment>();
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.postedAt = new Date();
+    }
+    
+    public Post addComment(String author, String content) {
+        Comment newComment = new Comment(this, author, content).save();
+        this.comments.add(newComment);
+        this.save();
+        return this;
+    }
+ 
+}
