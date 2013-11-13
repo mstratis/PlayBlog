12619ef3d2d4591babd436a42bda04e411594516
Mark Stratis <mstratis@cardinalsolutions.com>
2013-11-13 15:21:48 -0500

    Added custom home page

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -9,8 +9,18 @@ import models.*;
 
 public class Application extends Controller {
 
+//	@Before
+//	static void addDefaults() {
+//	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+//	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+//	}
+	
     public static void index() {
-        render();
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
 }
