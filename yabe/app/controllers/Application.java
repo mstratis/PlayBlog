a50e69ea1d23bd140e907467266de89bc3e46cf8
Mark Stratis <mstratis@cardinalsolutions.com>
2013-12-02 11:26:41 -0500

    added captcha functionality to the comments

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import play.*;
 import play.data.validation.*;
 import play.mvc.*;
+import play.libs.*;
+import play.cache.*;
 
 import java.util.*;
 
@@ -26,17 +28,33 @@ public class Application extends Controller {
 
     public static void show(Long id) {
         Post post = Post.findById(id);
-        render(post);
+        String randomID = Codec.UUID();
+        render(post, randomID);
     }
     
-    public static void postComment(Long postId, @Required String author, @Required String content) {
+    public static void postComment(
+            Long postId, 
+            @Required(message="Author is required") String author, 
+            @Required(message="A message is required") String content, 
+            @Required(message="Please type the code") String code, 
+            String randomID) 
+    {
         Post post = Post.findById(postId);
+        validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
         if(validation.hasErrors()) {
-            render("Application/show.html", post);
+            render("Application/show.html", post, randomID);
         }
         post.addComment(author, content);
         flash.success("Thanks for posting %s", author);
+        Cache.delete(randomID);
         show(postId);
+    }
+    
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
     }
     
 }
